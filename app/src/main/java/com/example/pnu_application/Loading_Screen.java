ef6ddaef68835781f5fb81f65c0fa04685ac37ee
mythaouel <thaottm19411@st.uel.edu.vn>
2021-10-29 00:52:48 +0700

    Thêm màn hình Loading (2.5s)_màn hình sau là home

---
+++ app/src/main/java/com/example/pnu_application/Loading_Screen.java
@@ -0,0 +1,29 @@
+package com.example.pnu_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Loading_Screen extends AppCompatActivity {
+
+    //Thời gian chờ là 2.5s
+    int SPLASH_TIME_OUT = 2500;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Home_Sreen.class - màn hình sau khi load xong
+                Intent intent = new Intent(Loading_Screen.this, Home_Screen.class);
+                startActivity(intent);
+
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
