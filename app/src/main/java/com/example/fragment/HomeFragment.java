c0420b396cbc55d24c1bb3ecfd036891f46f0c40
kimngan26301uel <91685108+kimngan26301uel@users.noreply.github.com>
2021-10-29 11:44:09 +0700

    match fragment với navbar
    match fragment với navbar

---
+++ app/src/main/java/com/example/fragment/HomeFragment.java
@@ -0,0 +1,51 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.adapter.BannerAdapter;
+import com.example.model.Banner;
+import com.example.pnu_application.R;
+
+import java.util.ArrayList;
+
+import me.relex.circleindicator.CircleIndicator3;
+
+
+public class HomeFragment extends Fragment {
+
+    private ViewPager2 nViewPager2;
+    private CircleIndicator3 nCircleIndicator3;
+    BannerAdapter bannerAdapter;
+    ArrayList<Banner> banners;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+        nViewPager2=view.findViewById(R.id.vpBanner);
+        nCircleIndicator3=view.findViewById(R.id.circleIndicator);
+        initData();
+        return view;
+
+    }
+
+    private void initData() {
+        ArrayList<Banner> banners=new ArrayList<>();
+        banners.add(new Banner(R.drawable.banner_1));
+        banners.add(new Banner(R.drawable.banner_2));
+        banners.add(new Banner(R.drawable.banner_3));
+        banners.add(new Banner(R.drawable.banner_4));
+
+        bannerAdapter=new BannerAdapter(getContext(),banners);
+        nViewPager2.setAdapter(bannerAdapter);
+        nCircleIndicator3.setViewPager(nViewPager2);
+
+
+    }
+}
--- app/src/main/java/com/example/pnu_application/MainActivity.java
+++ app/src/main/java/com/example/pnu_application/MainActivity.java
@@ -1,19 +1,60 @@
 package com.example.pnu_application;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.fragment.AccountFragment;
+import com.example.fragment.CartFragment;
+import com.example.fragment.CategoryFragment;
+import com.example.fragment.HomeFragment;
+import com.example.fragment.NotificationFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private TabLayout tabCategory;
+    private BottomNavigationView bottomNavigationView;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.navContainer);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new HomeFragment()).commit();
+        bottomNavigationView.setOnItemSelectedListener(navListener);
     }
+    private NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=null;
+            switch (item.getItemId()) {
+                case R.id.itHome:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.itCategory:
+                    selectedFragment = new CategoryFragment();
+                    break;
+                case R.id.itCart:
+                    selectedFragment = new CartFragment();
+                    break;
+                case R.id.itNotification:
+                    selectedFragment = new NotificationFragment();
+                    break;
+                case R.id.itAccount:
+                    selectedFragment = new AccountFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,selectedFragment).commit();
+            return true;
+        }
+
+    };
 }
