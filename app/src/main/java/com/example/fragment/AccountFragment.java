0a5f37618130e402b4ec422b6f1ba5a477abbd31
lnghle306uel <lelnh19411@st.uel.edu.vn>
2021-12-23 00:25:53 +0700

    xóa ds cart khi đăng xuất

--- app/src/main/java/com/example/fragment/AccountFragment.java
+++ app/src/main/java/com/example/fragment/AccountFragment.java
@@ -46,6 +46,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import utils.Constant;
 
 
 public class AccountFragment extends Fragment {
@@ -271,6 +272,7 @@ public class AccountFragment extends Fragment {
                 getActivity().finishAndRemoveTask();
                 Intent intent = new Intent(getContext(), MainActivity.class);
                 startActivity(intent);
+                Constant.arrCartProduct.clear();
             }
         });
         dialog.show();
